828a2141e1ff82646af4677c527b16708f62de8d
Rashedul-Alam-Rudra <rashedulalam.iit.du@gmail.com>
2024-03-21 17:39:04 +0200

    added sidebar menu items dynamic

--- src/app/components/map/map.component.ts
+++ src/app/components/map/map.component.ts
@@ -4,30 +4,47 @@ import * as L from 'leaflet';
 @Component({
   selector: 'app-map',
   templateUrl: './map.component.html',
-  styleUrl: './map.component.scss'
+  styleUrl: './map.component.scss',
 })
 export class MapComponent implements AfterViewInit {
   private map: any;
 
   private initMap(): void {
     this.map = L.map('map', {
-      center: [ 60.454510, 22.264824 ],
-      zoom: 6
+      center: [60.45451, 22.264824],
+      zoom: 6,
     });
 
-    const tiles = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      maxZoom: 18,
-      minZoom: 3,
-      attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-    });
+    const tiles = L.tileLayer(
+      'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+      {
+        maxZoom: 18,
+        minZoom: 3,
+        attribution:
+          '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+      }
+    );
 
     tiles.addTo(this.map);
+
+    const icon = {
+      icon: L.icon({
+        iconSize: [ 25, 41 ],
+        iconAnchor: [ 13, 0 ],
+        // specify the path here
+        iconUrl: './node_modules/leaflet/dist/images/marker-icon.png',
+        shadowUrl: './node_modules/leaflet/dist/images/marker-shadow.png'
+     })
+  };
+    L.marker([60.45451, 22.264824])
+      .addTo(this.map)
+      .bindPopup('I current live on this city.')
+      .openPopup();
   }
 
-  constructor() { }
+  constructor() {}
 
   ngAfterViewInit(): void {
     this.initMap();
   }
-
 }
--- src/app/shared/components/sidebar/sidebar.component.ts
+++ src/app/shared/components/sidebar/sidebar.component.ts
@@ -3,8 +3,29 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
-  styleUrl: './sidebar.component.scss'
+  styleUrl: './sidebar.component.scss',
 })
 export class SidebarComponent {
-
+  menuItems = [
+    {
+      name: 'Simple Map',
+      id: 'simple-map',
+      icon: 'home',
+    },
+    {
+      name: 'Map with Tools',
+      id: 'map-with-tools',
+      icon: 'other',
+    },
+    {
+      name: 'Map with Layers',
+      id: 'map-with-layers',
+      icon: 'other',
+    },
+    {
+      name: 'Map with Clustering',
+      id: 'map-with-clustering',
+      icon: 'other',
+    },
+  ];
 }
