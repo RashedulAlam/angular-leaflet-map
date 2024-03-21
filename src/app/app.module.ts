3c17cff013037b0399e6e98fbc931de14d6cf6af
Rashedul-Alam-Rudra <rashedulalam.iit.du@gmail.com>
2024-03-21 17:01:09 +0200

    initialized the maps

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
+import { MapComponent } from './components/map/map.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, MapComponent],
   imports: [BrowserModule, AppRoutingModule, SharedModule],
   providers: [],
   bootstrap: [AppComponent],
---
+++ src/app/components/map/map.component.ts
@@ -0,0 +1,33 @@
+import { AfterViewInit, Component } from '@angular/core';
+import * as L from 'leaflet';
+
+@Component({
+  selector: 'app-map',
+  templateUrl: './map.component.html',
+  styleUrl: './map.component.scss'
+})
+export class MapComponent implements AfterViewInit {
+  private map: any;
+
+  private initMap(): void {
+    this.map = L.map('map', {
+      center: [ 60.454510, 22.264824 ],
+      zoom: 6
+    });
+
+    const tiles = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      maxZoom: 18,
+      minZoom: 3,
+      attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    });
+
+    tiles.addTo(this.map);
+  }
+
+  constructor() { }
+
+  ngAfterViewInit(): void {
+    this.initMap();
+  }
+
+}
