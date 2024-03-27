f0bbe713d81c8dcd5a6c5b2ee5841a6c7bb9628d
Rashedul-Alam-Rudra <rashedulalam.iit.du@gmail.com>
2024-03-28 01:09:45 +0200

    added unit test for components

--- src/app/components/map-clusters/map-clusters.component.spec.ts
+++ src/app/components/map-clusters/map-clusters.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { MapClustersComponent } from './map-clusters.component';
+import { IMapConfig } from '../../models/map';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
 
 describe('MapClustersComponent', () => {
   let component: MapClustersComponent;
@@ -8,10 +9,12 @@ describe('MapClustersComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [MapClustersComponent]
-    })
-    .compileComponents();
-    
+      declarations: [MapClustersComponent],
+      schemas: [NO_ERRORS_SCHEMA],
+    }).compileComponents();
+  });
+
+  beforeEach(() => {
     fixture = TestBed.createComponent(MapClustersComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -19,5 +22,18 @@ describe('MapClustersComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should initialize mapConfig correctly', () => {
+    const expectedConfig: IMapConfig = {
+      addDrawingTool: true,
+      addOnClick: false,
+      addDefaultmarker: false,
+      addDefaultLayer: false,
+      addWmslayers: false,
+      addMarkerClusters: true,
+      useUSACenter: true,
+    };
+    expect(component.mapConfig).toEqual(expectedConfig);
   });
 });
